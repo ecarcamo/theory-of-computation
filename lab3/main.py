c6801c5e7a0f9696df9ca495b2cb8c6a9f8e9d09
ecarcamo <estebancarcamou@gmail.com>
2025-08-03 19:56:28 -0600

    lógica principal del lab

---
+++ lab3/main.py
@@ -0,0 +1,60 @@
+from shunting_yard import ShuntingYard
+from ast_builder import Ast_Builder
+from tree_visualizer import TreeVisualizer
+from balance_verifier import BalanceVerifier
+import os
+
+def main():
+    archivo = "data/expressions.txt"
+    
+    os.makedirs("output/", exist_ok=True)
+    
+    shunting_yard = ShuntingYard()
+    ast_tree = Ast_Builder()
+    balance_verifier = BalanceVerifier()
+    
+    
+    try:
+        with open(archivo, 'r', encoding='utf-8') as file:
+            expresiones = [linea.strip() for linea in file if linea.strip() and not linea.startswith('#')]
+        
+        print(f"Procesando {len(expresiones)} expresiones desde {archivo}\n")
+        
+        for i, expresion in enumerate(expresiones):
+            print(f"{'='*60}")
+            print(f"Expresión {i + 1}: {expresion}")
+            print(f"{'-'*60}")
+            
+            # usaremos el ejercicio2 del lab 2 para verificar el balanceo
+            balanceado, pasos_balanceo = balance_verifier.verificar_balanceo(expresion)
+            if balanceado:
+                print("Expresión balanceada")
+                # Convertir a postfix, igual que el lab 2
+                postfix, pasos = shunting_yard.convertir_a_postfix(expresion)
+                
+                # Mostrar pasos de conversión
+                for paso in pasos:
+                    print(paso)
+                
+                if postfix:
+                    print(f"\nResultado postfix: {postfix}")
+                    
+                    # Empieza el proceso de contrucción del AST
+                    ast = ast_tree.build_ast(postfix)
+                    if ast:
+                        # lo visualizamos en un png
+                        visualizer = TreeVisualizer(ast)
+                        output_path = f"output/ast_{i + 1}"
+                        visualizer.save(output_path)
+                    else:
+                        print("Error: No se pudo construir el AST")
+                else:
+                    print("Error en la conversión a postfix")
+    
+    except FileNotFoundError:
+        print(f"Error: No se encontró el archivo '{archivo}'")
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+
+if __name__ == "__main__":
+    main()
